3178f279b21fc70c26a550e5a4a317b4eea5574e
Josue Ortega <josue@debian.org>
2019-04-02 17:18:45 -0600

    Add exception management for twython

--- sopel/modules/twitter.py
+++ sopel/modules/twitter.py
@@ -21,9 +21,14 @@ def twit(bot, trigger):
     groups = trigger.group(0)
     s_groups = groups.split('/')
     t_uid = s_groups[len(s_groups)-1:][0]
-    bot.say(get_text(bot, t_uid))
+    twit = get_text(bot, t_uid)
+    if twit is not None:
+        bot.say(get_text(bot, t_uid))
 
 def get_text(bot, twit_uid):
     client = Twython(bot.config.twitter.public, bot.config.twitter.secret)
-    twit = client.show_status(id=twit_uid, tweet_mode='extended')
-    return twit.get('full_text')
+    try:
+        twit = client.show_status(id=twit_uid, tweet_mode='extended')
+        return twit.get('full_text')
+    except:
+        return None
