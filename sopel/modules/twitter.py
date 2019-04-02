5db7e11430490d976ff0629fc68a0680725b4631
Josue Ortega <josue@debian.org>
2019-04-02 17:12:25 -0600

    Fix regex parse and wrong references

--- sopel/modules/twitter.py
+++ sopel/modules/twitter.py
@@ -1,27 +1,29 @@
 # coding=utf-8
 
 """
-twitter.py - API-key-less Instagram module for Sopel
+twitter.py - API-key-less twitter module for Sopel
 Copyright 2019, Josue Ortega
 """
 
 import re
 
 from twython import Twython
+from sopel import module
 
 twitter_regex = r'((http|https)://(twitter.com)/(([A-Za-z0-9_]{1,15})/status)/([\d]*))'
 twitter_pattern = re.compile(twitter_regex)
 
 def setup(bot):
-    bot.register_url_callback(instagram_pattern, twit)
+    bot.register_url_callback(twitter_pattern, twit)
 
 @module.rule(twitter_regex)
 def twit(bot, trigger):
     groups = trigger.group(0)
-    t_uid = groups[-1]
-    bot.say(get_text(twit_uid))
+    s_groups = groups.split('/')
+    t_uid = s_groups[len(s_groups)-1:][0]
+    bot.say(get_text(bot, t_uid))
 
-def get_text(twit_uid):
+def get_text(bot, twit_uid):
     client = Twython(bot.config.twitter.public, bot.config.twitter.secret)
     twit = client.show_status(id=twit_uid, tweet_mode='extended')
     return twit.get('full_text')
