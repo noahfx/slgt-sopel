d2090ab588be979de954e488efc99b42edb87b2a
Josue Ortega <josue@debian.org>
2019-04-08 21:38:17 -0600

    Add persistance to joint module

--- sopel/modules/joint.py
+++ sopel/modules/joint.py
@@ -8,24 +8,35 @@ Copyright 2019, Josue Ortega
 
 from sopel import module
 
-PASS_JOINT = '%s le da el ultimo toque al porro y se lo pasa a %s, %d han sido rolados en este canal'
-SELF_JOINT = '%s prende un bate. %d han sido encendidos en este canal'
+PASS_JOINT = '%s le da el ultimo toque al porro y se lo pasa a %s, %d porros han sido rolados en este canal'
+SELF_JOINT = '%s prende un bate. %d porros han sido encendidos en este canal'
 COMMAND_NAME = 'porro'
 
+
+def initialize_db(bot, channel, key):
+    if bot.db.get_channel_value(channel, key) is None:
+        bot.db.set_channel_value(channel, key, 1)
+    return
+
+def update_db(bot, channel, key):
+    value = int(bot.db.get_channel_value(channel, key)) 
+    new_val = value + 1
+    bot.db.set_channel_value(channel, key, new_val)
+    return new_val
+
 def setup(bot):
-    if 'joint' not in bot.memory:
-        bot.memory = {'lighted': 0, 'passed': 0}
+    for c in bot.channels:
+        for k in ['passed', 'lit']:
+            initialize_db(bot=bot, channel=c, key=k)
 
 @module.commands(COMMAND_NAME)
 @module.nickname_commands(COMMAND_NAME)
 def joint(bot, trigger):
     payload = trigger.group(2)
     if payload is None:
-        bot.memory['joint']['lighted'] += 1 
-        joints = bot.memory.get('joint', {}).get('lighted', 1)
-        bot.reply(SELF_JOINT % (bot.nick, int(joints)))
+        joints = update_db(bot, trigger.sender, 'lit')
+        bot.reply(SELF_JOINT % (trigger.nick, joints))
         return
-    bot.memory['joint']['passed'] += 1 
-    joints = bot.memory.get('joint', {}).get('passed', 1)
-    bot.reply(PASS_JOINT % (bot.nick, payload, int(joints)))
+    joints = update_db(bot, trigger.sender, 'passed')
+    bot.say(PASS_JOINT % (trigger.nick, payload, joints))
     return
